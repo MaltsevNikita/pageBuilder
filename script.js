55aaf4fd90f29040f5058c971ccc76cf38c5f227
MaltsevNikita <mn121288@gmail.com>
2024-04-25 15:33:18 +0300

    Сделал разные секции вёрстки и указал везде приблизительные цены

--- script.js
+++ script.js
@@ -5,16 +5,27 @@
 // текст + слайдер  type4
 // текст + квиз     type5
 
+let prices = {
+  type1:100,
+  type2:300,
+  type3:500,
+  type4:800,
+  type5:1000,
+  type6:800,
+  type7:800,
+  type8:1000,
+  type9:1500,
+}
 document.addEventListener('DOMContentLoaded', function() {
   const container = document.querySelector('.container');
   const addSelectBtn = document.getElementById('addSelect');
   const clearAllBtn = document.getElementById('clearAll');
   const countBtn = document.getElementById('count');
-  const total = document.getElementById('total')
+  // let total = document.getElementById('total')
 
   countBtn.addEventListener('click', () => {
     countTotal();
-    console.log('hello');
+    // console.log('hello');
   })
 
   addSelectBtn.addEventListener('click', addSelectBlock);
@@ -25,11 +36,15 @@ document.addEventListener('DOMContentLoaded', function() {
     block.classList.add('block');
     block.innerHTML = `
       <select class="selectType">
-        <option value="type1" data-image-url="img/types/type1.jpg">секция текст</option>
-        <option value="type2" data-image-url="img/types/type2.jpg">секция текст + каринка</option>
-        <option value="type3" data-image-url="img/types/type3.jpg">секция текст + галерея</option>
-        <option value="type4" data-image-url="img/types/type4.jpg">секция текст + слайдер</option>
-        <option value="type5" data-image-url="img/types/type5.jpg">секция текст + квиз</option>
+        <option value="type1" data-image-url="img/types/block_imgText.jpg">картинка + текст</option>
+        <option value="type2" data-image-url="img/types/block_Galery.jpg">галлерея</option>
+        <option value="type3" data-image-url="img/types/block_slider.jpg">слайдер</option>
+        <option value="type4" data-image-url="img/types/block_Toggle.jpg">список с маркерами</option>
+        <option value="type5" data-image-url="img/types/block_Quiz.jpg">квиз</option>
+        <option value="type6" data-image-url="img/types/block_Counter.jpg">счётчик</option>
+        <option value="type7" data-image-url="img/types/block_Posts.jpg">секция с миниатюрами постов</option>
+        <option value="type8" data-image-url="img/types/block_Video.jpg">видео</option>
+        <option value="type9" data-image-url="img/types/block_Maps.jpg">карта</option>
       </select>
       <button class="removeSelect">-</button>
       <button class="moveUp">↑</button>
@@ -80,10 +95,22 @@ document.addEventListener('DOMContentLoaded', function() {
       selectValues.push(select.value); // Retrieve and store the value of each select element
     });
 
+    let total = 0;
+
     selectValues.forEach(selectedItem => {
-      console.log(selectedItem);
+      // alert(selectedItem)
+      // console.log(selectedItem);
+      // console.log(typeof(prices[selectedItem]) );
+      total += prices[selectedItem];
+      
+
     })
-    // console.log(selectValues[2]);
+
+    console.log(total);
+    // console.log(selectedItem.selectedIndex);
+    // console.log(prices.);
+    
+    // console.log(selectValues);
     // count.value = selectValues[2];
 
   }
